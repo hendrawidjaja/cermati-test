3561013a43680b0cd4d64f60fd1195830e56673f
Hendra Widjaja <hendrawidjaja@live.de>
2021-06-14 11:03:35 +0700

    added transition for notification-panel

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-window.onscroll = function (e) {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    // you're at the bottom of the page
-    console.log(e);
-  }
-};
-
 const btnCloseNotification = document.querySelector(".btn-notification-panel");
 const btnCloseNewsLetter = document.querySelector(".btn-close");
 const notificationPanel = document.querySelector(".section-notification-panel");
 const newsLetterPanel = document.querySelector(".section-newsletter-panel");
+
+window.onscroll = function (e) {
+  const screenHeight = document.body.offsetHeight - window.innerHeight;
+  const screenHeight33 = screenHeight / 3;
+
+  if (window.scrollY >= screenHeight33) {
+    newsLetterPanel.classList.add("reveal");
+  }
+};
 
 function removeNode(selector, classList = "hide", timeout = 6000) {
   selector.classList.add(classList);
